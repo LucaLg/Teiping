a6339fe09684f9328d0cdfcbc504f8bdd9397015
Luca <luca@lucas-air.speedport.ip>
2024-01-24 19:51:42 +0100

    refactor(timer): manage subscription with takeUntilDestroy instead of onDestroy

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, DestroyRef, inject, input } from '@angular/core';
 import { Subscription, timer } from 'rxjs';
 import { TimeFormatPipe } from '../time-format.pipe';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-timer',
@@ -10,24 +11,21 @@ import { TimeFormatPipe } from '../time-format.pipe';
   styles: ``,
 })
 export class TimerComponent {
+  private destroyRef = inject(DestroyRef);
   time = input.required<number>();
-  private timerSubscription: Subscription = new Subscription();
+  private timer$: Subscription = new Subscription();
   public timerValue: number = 0;
   interval = 10;
 
   startTimer() {
-    this.timerSubscription = timer(0, 10).subscribe(() => {
-      if (this.timerValue < this.time()) {
-        this.timerValue += this.interval;
-      } else {
-        this.timerSubscription.unsubscribe(); // Stop the timer when it reaches 60 seconds
-      }
-    });
-  }
-
-  ngOnDestroy() {
-    if (this.timerSubscription) {
-      this.timerSubscription.unsubscribe(); // Unsubscribe to avoid memory leaks
-    }
+    this.timer$ = timer(0, 10)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        if (this.timerValue < this.time()) {
+          this.timerValue += this.interval;
+        } else {
+          this.timer$.unsubscribe(); // Stop the timer when it reaches 60 seconds
+        }
+      });
   }
 }
